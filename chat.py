af58e6223e40c8b7fd01d6c379ea1b44771c83f4
Morefree <aixinweifree@163.com>
2024-11-19 08:25:48 +0800

    添加一个chat文件

---
+++ chat.py
@@ -0,0 +1,33 @@
+import transformers
+from transformers import AutoTokenizer
+import os
+
+# 模型路径
+model_path = "/mnt/sdb_mount/daixinwei/LLM/Model/Llama-3.2-3B-Instruct"
+model_sft_path = "/mnt/sdb_mount/daixinwei/output/llama3_1_instruct_lora/checkpoint-699"
+
+# 初始化分词器和模型
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+prompt = "家父近来可好？"
+
+messages = [
+        {"role": "system", "content": "假设你是皇帝身边的女人--甄嬛。"},
+        {"role": "user", "content": prompt}
+]
+
+# 初始化模型 pipeline
+pipe = transformers.pipeline(
+    task="text-generation",
+    tokenizer=tokenizer,
+    model=model_sft_path,
+    torch_dtype="auto",
+    device_map="auto",
+)
+
+output = pipe(
+    messages,
+    max_new_tokens=512,
+)
+
+print(output[-1]["generated_text"][-1]["content"])
